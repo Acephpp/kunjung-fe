6a304971cb1448e1cbf427cf9467b9eee24dd7cd
AcepHp <acephandika27@gmail.com>
2025-09-16 12:10:05 +0700

    fix: definingComport fixes

--- frontend/src/components/Homepage/DefiningComport.tsx
+++ frontend/src/components/Homepage/DefiningComport.tsx
@@ -8,18 +8,18 @@ export default function DefiningComport() {
 
                 </div>
                 <div>
-                    <p className="text-sm font-bold text-gray-800 mb-15">
+                    <p className="text-xl font-bold text-gray-800 mb-[180px]">
                         KUNJUNG Family
                     </p>
                 </div>
 
                 {/* Kiri atas */}
                 <div>
-                    <h2 className="text-3xl font-bold text-gray-900 mb-4 leading-snug">
+                    <h2 className="text-[40px] font-bold text-gray-900 mb-4 leading-snug">
                         Defining Comfort
                     </h2>
-                    <p className="text-gray-600">
-                        Redefine Comfort, Discover <br /> One’s Self
+                    <p className="text-gray-600 text-xl">
+                        Redefine Comfort, Discover <br /> One's Self
                     </p>
                 </div>
 
@@ -27,11 +27,11 @@ export default function DefiningComport() {
                 <div>
                     {/* Tambahkan teks KUNJUNG Family */}
                     
-                    <p className="text-gray-700 leading-relaxed">
+                    <p className="text-gray-700 text-sm leading-relaxed">
                         A home of unparalleled connections, offering a journey of transformation
                         through accessible artistry.
                         <br /><br />
-                        Kunjung was created to be more than a place to stay; it’s a place to belong.
+                        Kunjung was created to be more than a place to stay; it's a place to belong.
                         Here, every visit is a chance to find warmth, inspiration, and comfort in a
                         space that feels like home. Our team welcomes each guest as part of our story,
                         crafting moments that stay with you long after you leave.
